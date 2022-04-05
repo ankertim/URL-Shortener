55018e15f3c90849a6ddc03bbcbbf92b2c8afb4b
ankertim <yyho.swe@gmail.com>
2022-04-05 21:18:31 +0800

    urlPage can post but have one problem
    Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const req = require('express/lib/request');
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var global_a;
 
 /* GET home page. */
-function indexf(req, res, next) {
-    res.render('index', { title: '生成短網址工具' });
+function indexPage(req, res, next) {
+    res.render('index', { title: '生成短網址網站' });
 }
-router.get('/', indexf);
+router.get('/', indexPage);
 
-module.exports = router;
-// database test
+/* GET shorturl page */
+function get_urlPage(req, res, next) {
+    res.render('urlPage', { title: '短網址生成工具' });
+}
+router.get('/api/urls', get_urlPage);
+/*
+// database setting
 var mysql = require('mysql');
 var db_info = {
     host: 'localhost',
@@ -21,15 +27,66 @@ var db_info = {
 var connection = mysql.createConnection(db_info);
 connection.connect();
 var query = {
-    sql: 'SELECT * FROM shorturl',
-    timeout: 40000,
+    sql: 'SELECT * FROM shorturl'
 }
 function callback(error, rows, fields) {
     if (error) throw error;
+    global_a = rows;
     console.log('The result is: ', rows);
     console.log('type of result is: ', typeof(rows));
 }
 connection.query(query, callback);
+console.log('The result: ', global_a);
 connection.end();
-// use pool
+*/
+//===========================================================
+// create pool
+var mysql = require('mysql');
+var db_info = {
+  host: 'localhost',
+  user: 'ankertim',
+  password: '1234',
+  database:'dcard_intern',
+  port: 3306,
+  dateStrings: true
+}
 var pool = mysql.createPool(db_info);
+
+// function  
+function urlCallback(req, res) {
+  console.log('Hello AAAA: ', req.body.orig_url);
+  var query = {
+    sql: 'SELECT * FROM shorturl'
+    //url: req.body.orig_url
+  }
+  
+  function dbCallback(error, results, fields) {
+    if (error) throw error;
+    console.log('The result is: ', results);
+    console.log('Hello AAA: ');
+  }
+
+  pool.getConnection(
+    function(err, connection) { 
+      connection.query(query, dbCallback) //(error, rows, fields)
+    }
+  );
+  /*
+  //新增完成後，查詢目前所有使用者
+  connection.query('select * from users', function(err, rows, field){
+      if (err)
+          throw err;
+          console.log('Hello BBB: ');
+      //將資料傳送到首頁的使用者列表
+      res.render('user', {
+          title: '使用者列表',
+          user: rows
+      });
+  });
+  */
+  res.render('urlPage', { title: '短網址生成工具' });
+  connection.release();
+}
+router.post('/convert', urlCallback)
+
+module.exports = router;
